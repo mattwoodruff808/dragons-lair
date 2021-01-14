081cd672a7f243dfcaf5b2cd341d834a1b819250
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-14 14:08:48 -0700

    Now actually done with step 1

--- server/index.js
+++ server/index.js
@@ -2,9 +2,23 @@ require('dotenv').config();
 const express = require('express'),
       session = require('express-session'),
       massive = require('massive'),
-      app = express(),
-      PORT = 4000;
+      {CONNECTION_STRING, SESSION_SECRET} = process.env,
+      app = express();
 
 app.use(express.json());
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+}))
 
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+})
+
+const PORT = 4000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
